cca052fb6b25701d774dc42332adea85db860f39
cynthialiang00 <cynliang00@gmail.com>
2023-02-20 22:52:25 -0800

    added ReviewImages, Review models&migrations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -44,7 +44,7 @@ module.exports = (sequelize, DataTypes) => {
       return await User.scope('currentUser').findByPk(user.id);
     }
     static associate(models) {
-      // define association here
+      User.hasMany(models.Review, { foreignKey: 'userId' });
     }
   };
 
