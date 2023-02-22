2a44510aeea17074df39e60b4d12e0a3f3c25d6e
cynthialiang00 <cynliang00@gmail.com>
2023-02-22 14:09:02 -0800

    get all spots working, without inclusions

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User, Spot } = require('../../db/models');
+
+const router = express.Router();
+
+// get all spots
+router.get('/', async (req,res) => {
+    const spots = await Spot.findAll();
+    res.json(spots)
+})
+
+module.exports = router;
