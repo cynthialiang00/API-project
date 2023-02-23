e65b0f0d60cca27bfce516d6a41799e231255f40
cynthialiang00 <cynliang00@gmail.com>
2023-02-23 10:33:31 -0800

    Get details of a Spot from id implemented

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -52,22 +52,118 @@ router.get('/', async (req,res) => {
     res.json({Spots: spotObjects });
 })
 
- // create a spot 
- // AUTH: True VALIDATION : True
+// get all spots owned by current user
+// AUTH: True 
+router.get('/current', requireAuth, restoreUser, async (req,res) => {
+    const { user } = req;
+    const spots = await Spot.findByPk(user.id, {
+        include: [
+            {
+                model: SpotImage,
+                attributes: ['url', 'preview']
+            }
+        ]
+    });
 
+    const spotObjects = [];
+    spots.length ? 
+    spotsArray.forEach(spot => spotObjects.push(spot.toJSON()))
+    : spotObjects.push(spots.toJSON());
+
+    for (let spot of spotObjects) {
+        const review = await Review.findOne({
+            where: {
+                spotId: spot.id
+            },
+            attributes: [
+                [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+            ]
+        })
+        if (review) {
+            spot.avgRating = review.toJSON().avgRating;
+        }
+        else spot.avgRating = "No Reviews exist for this spot";
+
+        if (spot.SpotImages.length) {
+            const filterTrue = spot.SpotImages.filter(image => image.preview === true);
+            filterTrue.length ? spot.previewImage = filterTrue[0].url : spot.previewImage = "No Preview Image Available";
+        }
+        else {
+            spot.previewImage = "No Preview Image Available";
+        }
+        delete spot.SpotImages;
+    };
+
+    res.status = 200;
+    res.json({ Spots: spotObjects });
+    
+})
+
+// Get details of a Spot from an id
+// AUTH: False
+router.get('/:spotId', async (req, res, next) => {
+    const spot = await Spot.findByPk(req.params.spotId, {
+        include: [
+            {
+                model: SpotImage,
+                attributes: ['id', 'url', 'preview']
+            },
+            {
+                model: User,
+                attributes: ['id', 'firstName', 'lastName']
+            }
+        ]
+    });
+    
+    if (!spot) {
+        const err = new Error("Spot couldn't be found");
+        err.status = 404;
+        return next(err);
+    }
+    const spotObject = spot.toJSON();
+
+    const review = await Review.findOne({
+        where: {
+            spotId: spot.id
+        },
+        attributes: [
+            [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating'],
+            [sequelize.fn('COUNT', sequelize.col('stars')), 'numReviews']
+        ]
+    })
+    if (review) {
+        spotObject.numReviews = review.toJSON().numReviews;
+        spotObject.avgStarRating = review.toJSON().avgRating;
+    }
+    else {
+        spotObject.numReviews = null;
+        spotObject.avgRating = null;
+    }
+
+    if (spotObject.User) {
+        spotObject.Owner = spotObject.User;
+        delete spotObject.User;
+    }    
+    res.status = 200;
+    res.json(spotObject);
+})
+
+// create a spot 
+// AUTH: True VALIDATION : True
 router.post('/', requireAuth, restoreUser, validateCreateSpot, async (req, res) => {
     const { user } = req;
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
-        // console.log(`user id: `, user.id);
+    // console.log(`user id: `, user.id);
 
     const newSpot = await Spot.create({
         ownerId: user.id,
         address, city, state, country, lat, lng, name, description, price
     });
-    res.statusCode = 201;
+    res.status = 201;
     res.json(newSpot);
-    
-    
+
 })
 
+
+// 
 module.exports = router;
