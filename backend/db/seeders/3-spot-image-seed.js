283112ac78e17a1bc058521f5aa77dd0e44b019f
cynthialiang00 <cynliang00@gmail.com>
2023-02-22 23:13:50 -0800

    urls fixed for image seed

--- backend/db/seeders/3-spot-image-seed.js
+++ backend/db/seeders/3-spot-image-seed.js
@@ -16,12 +16,12 @@ module.exports = {
       },
       {
         spotId: 1,
-        url: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6e487c2a-00a9-462e-b2b6-462a7dfb8125/dee0k33-ec632282-aec6-43ab-b754-7d3b32f4ab72.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzZlNDg3YzJhLTAwYTktNDYyZS1iMmI2LTQ2MmE3ZGZiODEyNVwvZGVlMGszMy1lYzYzMjI4Mi1hZWM2LTQzYWItYjc1NC03ZDNiMzJmNGFiNzIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.abVzl7ltiZM_Wu1x41klIwF3yUsHv6FYVJL8eaFHQ2A",
+        url: "random1-url.com/random.jpg",
         preview: false
       },
       {
         spotId: 1,
-        url: "https://th-thumbnailer.cdn-si-edu.com/qwdFU8TzPixEtFzRLC2V_Ezr2tw=/fit-in/1600x0/https%3A%2F%2Ftf-cmsv2-smithsonianmag-media.s3.amazonaws.com%2Ffiler%2Fd5%2F24%2Fd5243019-e0fc-4b3c-8cdb-48e22f38bff2%2Fistock-183380744.jpg",
+        url: "random2-url.com/random.png",
         preview: true
       },
       {
