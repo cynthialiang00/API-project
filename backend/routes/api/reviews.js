f9efd3fd9e1fa07f7f51e360a68c09cfe9857fa7
cynthialiang00 <cynliang00@gmail.com>
2023-02-24 08:29:28 -0800

    Add an Image to a Review based on the Review's id with proper authorization and authentication

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -36,6 +36,49 @@ router.get('/current', requireAuth, restoreUser, async (req, res) => {
     res.json({Reviews: reviewObjects});
 });
 
+// Add an Image to a Review based on the Review's id
+// AUTHEN: true
+router.post('/:reviewId/images', requireAuth, restoreUser, async (req, res, next) => {
+    const { user } = req;
+    const { url } = req.body;
+
+    const review = await Review.findByPk(req.params.reviewId, {
+        include: [
+            {model: ReviewImage}
+        ]
+    })
+    if (!review) {
+        const err = new Error("Review couldn't be found");
+        err.status = 404;
+        return next(err);
+    }
+    if (review.userId !== user.id) {
+        const err = new Error('Forbidden');
+        err.status = 403;
+        return next(err);
+    }
+
+    const imageCount = review.ReviewImages.reduce( (count, img) => count+=1 , 0);
+    if (imageCount === 10 ) {
+        const err = new Error("Maximum number of images for this resource was reached");
+        err.status = 403;
+        return next(err);
+    }
+
+    const newImg = await ReviewImage.create({
+        reviewId: review.id,
+        url
+    })
+    const resImg = await ReviewImage.findByPk(newImg.id, {
+        attributes: ['id', 'url']
+    });
+
+    res.status(200);
+    res.json(resImg)
+
+
+})
+
 
 
 module.exports = router;
