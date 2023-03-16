09bdf3b64ba4c9e9d2006084b71230c891183e8c
cynthialiang00 <cynliang00@gmail.com>
2023-03-16 14:35:39 -0700

    Review State Store and reducer implemented

---
+++ frontend/src/components/Reviews/ReadReviews.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as spotActions from '../../store/spot';
+import { useEffect, useState, useRef } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import OpenReserveMenuItem from "./OpenReserveMenuItem";
+import ReserveFormModal from "./ReserveFormModal";
+
+function ReadReviews () {
+
+}
+
+export default ReadReviews;
---
+++ frontend/src/store/review.js
@@ -0,0 +1,50 @@
+import { csrfFetch } from './csrf';
+
+const GET_RVWS = "spot/GET_RVWS";
+
+const actionGetRvws= (rvws) => {
+    return {
+        type: GET_RVWS,
+        payload: rvws,
+    };
+};
+
+export const thunkGetRvws = (spotId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/spots/${spotId}/reviews`, {
+        method: 'GET',
+    });
+
+    const data = await response.json();
+    dispatch(actionGetRvws(normalizeData(data.Reviews)));
+    return response;
+};
+
+const initialState = { spot: {}, user: {} };
+
+const reviewReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case GET_RVWS:
+            newState = Object.assign({}, state);
+            newState.spot = {};
+            newState.spot = action.payload;
+            return newState;
+        
+        default:
+            return state;
+    }
+};
+
+
+// Function takes in an array of data and normalizes it based on data element's id
+// Returns the normalized data as an object
+const normalizeData = (dataArr) => {
+    const normalizeObj = {};
+    dataArr.forEach(element => {
+        normalizeObj[element.id] = element
+    });
+
+    return normalizeObj;
+
+}
+export default reviewReducer;
