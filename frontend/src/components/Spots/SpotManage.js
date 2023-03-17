bbc7c4129a86084c8ce42c83cc354b7fe7d43fc7
cynthialiang00 <cynliang00@gmail.com>
2023-03-17 15:40:07 -0700

    SpotManage.js deleted return statement

--- frontend/src/components/Spots/SpotManage.js
+++ frontend/src/components/Spots/SpotManage.js
@@ -19,53 +19,6 @@ function SpotManage() {
     const allSpotsArr = Object.values(allSpotsObj);
 
 
-
-    return (
-        <div className="spots-content">
-            <div className="spots-page-header">
-                <h2>Manage Your Spots</h2>
-                <button className="small-button" >
-                    <NavLink className="button-link" to="/spots/new">Create a New Spot</NavLink>
-                </button>
-            </div>
-            <div className="spots-grid">
-                {allSpotsArr.map((spot) => (
-                    <div key={spot.id} className="spot-container" title={`${spot.city}, ${spot.state}`}>
-                        <NavLink exact to={`/spots/${spot.id}`} className="spot-link">
-                            <img className="spot-image" src={`${spot.previewImage}`} alt={`Preview of ${spot.address}`}></img>
-                            <div className="spot-description">
-                                <div className="spot-title">{`${spot.city}, ${spot.state}`}</div>
-                                <div className="spot-price">{`$${spot.price} night`}</div>
-                                <span className="avg-rating-container">
-                                    <span className="avg-rating-star">
-                                        <i className="fa-solid fa-star"></i>
-                                    </span>
-                                    <span className="avg-rating-rating">
-                                        {spot.avgRating === "No Reviews exist for this spot" ? `New` : `${spot.avgRating}`}
-                                    </span>
-
-                                </span>
-                            </div>
-                        </NavLink>
-
-                        <div className="update-button">
-                            <button className="small-button" >
-                                <NavLink className="button-link" to={`${spot.id}/edit`}>Update</NavLink>
-                            </button>
-                        </div>
-                        <div className="update-button">
-                            <OpenModalButton
-                                buttonText="Delete"
-                                modalComponent={<DeleteModal id={spot.id} />}
-                            />
-                        </div>
-                    </div>
-
-                ))}
-            </div>
-        </div>
-
-    )
 }
 
 export default SpotManage;
