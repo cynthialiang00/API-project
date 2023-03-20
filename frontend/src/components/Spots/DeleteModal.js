e74a1570028bf430541fe8618cf848034fb6c559
cynthialiang00 <cynliang00@gmail.com>
2023-03-20 01:54:15 -0700

    Manage Spot Delete modal finalized css

--- frontend/src/components/Spots/DeleteModal.js
+++ frontend/src/components/Spots/DeleteModal.js
@@ -5,15 +5,20 @@ import { useDispatch } from 'react-redux';
 
 function DeleteModal({id}) {
     const dispatch = useDispatch();
+    const { closeModal } = useModal();
 
     const deleteTest = (e) =>{
         e.preventDefault();
         dispatch(spotActions.thunkDeleteSpot(id))
+        closeModal();
+
     }
     return (
-        <div>
-            <button>Go Back</button>
-            <button onClick={deleteTest}>Delete Spot</button>
+        <div className="delete-spot-parent">
+            <h2>Confirm Delete</h2>
+            <p>Are you sure you want to remove this spot from the listings?</p>
+            <button className="delete-spot-yes" onClick={deleteTest}>Yes (Delete Spot)</button>
+            <button className="delete-spot-no">Yes (Keep Spot)</button>
         </div>
     )
 }
