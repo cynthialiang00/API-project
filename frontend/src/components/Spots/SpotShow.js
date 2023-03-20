888264538857ab54e69208c0c48b8f15d00d8ed6
cynthialiang00 <cynliang00@gmail.com>
2023-03-20 05:59:41 -0700

    Fixed Grid margins in SpotShow.css

--- frontend/src/components/Spots/SpotShow.js
+++ frontend/src/components/Spots/SpotShow.js
@@ -39,25 +39,30 @@ function SpotShow() {
                             <p>{`${spot.city}, ${spot.state}, ${spot.country}`}</p>
                         </div>
                     </div>
-                        {spot.SpotImages.map((img) => (
-                            <div key={img.id} className="spot-images">
-                                <div className="main-img-container">
-                                    <img className="main-pic" src={img.url} alt="spot picture"></img>
-                                </div>
-                                <div className="img-1-container">
-                                    <img className= "small-pic1" src="https://visitkinosaki.com/vkcore/wp-content/uploads/2021/04/about-top-slider1-5.jpg"></img>
-                                </div>
-                                <div className="img-2-container">
-                                    <img className="small-pic2" src="https://photos.smugmug.com/Kyoto/Kinosaki-Onsen/i-JvX4wKT/0/a7cad879/L/shutterstock_645280435-L.jpg"></img>
-                                </div>
-                                <div className="img-3-container">
-                                    <img className="small-pic3" src="https://cdn.jalan.jp/jalan/images/pict3L/Y8/Y371928/Y371928268.jpg"></img>
-                                </div>
-                                <div className="img-4-container">
-                                    <img className="small-pic4" src="https://i0.wp.com/outoftownblog.com/wp-content/uploads/2020/01/Yukata-Rental-with-Sakura-1.jpg?fit=1000%2C1500&ssl=1"></img>
-                                </div>
+                    <div className="spot-images">
+                        {spot.SpotImages.map((img, index) => (
+                            
+                            <div key={img.id} className={`img-${index}-container`}>
+                                <img className={`pic-${index}`} src={img.url} alt="spot picture"></img>
                             </div>
+
+                            // <div className="img-1-container">
+                            //     <img className= "small-pic1" src="https://visitkinosaki.com/vkcore/wp-content/uploads/2021/04/about-top-slider1-5.jpg"></img>
+                            // </div>
+                            // <div className="img-2-container">
+                            //     <img className="small-pic2" src="https://photos.smugmug.com/Kyoto/Kinosaki-Onsen/i-JvX4wKT/0/a7cad879/L/shutterstock_645280435-L.jpg"></img>
+                            // </div>
+                            // <div className="img-3-container">
+                            //     <img className="small-pic3" src="https://cdn.jalan.jp/jalan/images/pict3L/Y8/Y371928/Y371928268.jpg"></img>
+                            // </div>
+                            // <div className="img-4-container">
+                            //     <img className="small-pic4" src="https://i0.wp.com/outoftownblog.com/wp-content/uploads/2020/01/Yukata-Rental-with-Sakura-1.jpg?fit=1000%2C1500&ssl=1"></img>
+                            // </div>
+
+
                         ))}
+                    </div>
+                        
                     
                     <div className="spot-details">
                         <div className="spot-details-head">
