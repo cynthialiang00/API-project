f49812919f5a4d6be2a75b5827d63ebc9507aeaf
cynthialiang00 <cynliang00@gmail.com>
2023-07-24 09:33:13 -0700

    user bookings page renders when no user bookings

--- frontend/src/components/Bookings/UserBookings/UserBookings.js
+++ frontend/src/components/Bookings/UserBookings/UserBookings.js
@@ -9,13 +9,12 @@ function UserBookings ({ user }) {
     const dispatch = useDispatch();
     const history = useHistory();
     const userBookings = useSelector(state => state.bookings.user);
-
+    
     useEffect(() => {
         dispatch(thunkGetUserBookings());
     }, [dispatch]);
 
     const userBookingsArr = Object.values(userBookings);
-
     
     if (!user) {
         return history.push("/not-found");
@@ -24,12 +23,15 @@ function UserBookings ({ user }) {
 
     return (
         <div className="user-bookings-wrapper">
-            {userBookingsArr.length > 0 ?
+            {userBookings.undefined ?
+                <h2> No Bookings </h2>
+                :
+
                 userBookingsArr.map((booking) => (
                     <UserBookingCard key={booking.id} booking={booking} user={user}/>
                 ))
-                :
-                <h2> No Bookings. </h2>
+                
+                
             }
         </div>
 
